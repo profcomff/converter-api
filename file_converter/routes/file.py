c47c45ffd7972135c459b7cabd166a64af1d70e2
nmkazantsev <nmkazantsev@gmail.com>
2023-03-25 20:41:27 +0300

    everythin works except unit tests

--- file_converter/routes/file.py
+++ file_converter/routes/file.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, File, UploadFile, Request
 from pydantic import BaseModel
 
 from file_converter.settings import Settings, get_settings
-from fastapi.params import Depends
+from fastapi.params import Depends, Form
 from fastapi.exceptions import HTTPException
 from file_converter.utils.convertable import check_pdf_ok, convert
 from file_converter.utils.commands import run
@@ -22,9 +22,9 @@ class Input(BaseModel):
 
 
 @router.post("/")
-async def upload_file(file: UploadFile, inp: Input, settings: Settings = Depends(get_settings)):
+async def upload_file(file: UploadFile = File(), to_ext : str = Form(default=None), settings: Settings = Depends(get_settings)):
     """Upload file to server. Takes extention to wich the file will be converted and the file"""
-    if not inp.to_ext in settings.CONVERT_TYPES:
+    if not to_ext in settings.CONVERT_TYPES:
         raise HTTPException(415, 'unsupported to_ext')
     if file.filename.split(".")[-1] not in settings.EXTENTIONS:
         raise HTTPException(
@@ -32,7 +32,7 @@ async def upload_file(file: UploadFile, inp: Input, settings: Settings = Depends
             f'Only {", ".join(settings.EXTENTIONS)} files allowed, but {file.content_type} recieved',
         )
 
-    result = await convert(file, inp.to_ext, settings.STATIC_FOLDER)
+    result = await convert(file, to_ext, settings.STATIC_FOLDER)
     if not await check_pdf_ok(result):
         await run(f"rm {result}")
         raise (HTTPException(413, "file corrupted"))
--- tests/test_routes/test_converter.py
+++ tests/test_routes/test_converter.py
@@ -7,64 +7,57 @@ settings = get_settings()
 
 
 def test_dock_success(client):
-    body = {
-        "to_ext": "pdf",
-    }
     fileName = 'tests/test_routes/test_files/test.docx'
     files = {
         'file': (
             f"{fileName}",
             open(f"{fileName}", 'rb'),
             "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-        )
+        ),
+        "to_ext": "pdf"
     }
-    res = client.post(url, params=body, files=files)
+    res = client.post(url, files=files)
     assert res.status_code == status.HTTP_200_OK
 
 
 def test_doc_success(client):
-    body = {
-        "to_ext": "pdf",
-    }
     fileName = 'tests/test_routes/test_files/test.doc'
     files = {
         'file': (
             f"{fileName}",
             open(f"{fileName}", 'rb'),
             "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-        )
+        ),
+        "to_ext": "pdf"
     }
-    res = client.post(url, params=body, files=files)
+    res = client.post(url, files=files)
     assert res.status_code == status.HTTP_200_OK
 
 
 def test_post_broken(client):
-    body = {
-        "to_ext": "pdf",
-    }
     fileName = 'tests/test_routes/test_files/test_broken.docx'
     files = {
         'file': (
             f"{fileName}",
             open(f"{fileName}", 'rb'),
             "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-        )
+        ),
+        "to_ext": "pdf"
+
     }
-    res = client.post(url, params=body, files=files)
+    res = client.post(url, files=files)
     assert res.status_code == status.HTTP_413_REQUEST_ENTITY_TOO_LARGE
 
 
 def test_post_unsupported_convert_type(client):
-    body = {
-        "to_ext": "asdfgh",
-    }
     fileName = 'tests/test_routes/test_files/test.docx'
     files = {
         'file': (
             f"{fileName}",
             open(f"{fileName}", 'rb'),
             "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-        )
+        ),
+        "to_ext": "pdf"
     }
-    res = client.post(url, params=body, files=files)
+    res = client.post(url,  files=files)
     assert res.status_code == status.HTTP_415_UNSUPPORTED_MEDIA_TYPE
