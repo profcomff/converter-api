d9d800de02eeb80ade90b0c8763be3fc5ea9156b
Konstantin <kosedo22@mail.ru>
2023-04-14 21:51:30 +0300

    upd
    Переопределил поведение __init__ в исключениях через super()

--- file_converter/exceptions.py
+++ file_converter/exceptions.py
@@ -1,8 +1,8 @@
 class ConvertError(Exception):
     def __init__(self):
-        self.message = 'Posted file is corrupted'
+        super().__init__('Posted file is corrupted')
 
 
 class ForbiddenExt(Exception):
     def __init__(self):
-        self.message = 'Posted file has an unsupported ext'
+        super().__init__('Unsupported ext')
